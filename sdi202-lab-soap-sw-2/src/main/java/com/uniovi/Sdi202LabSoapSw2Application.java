7358523e39886b3aba30e32647620ea8aad593e1
Elena <uo258525@uniovi.es>
2019-04-18 15:50:27 +0200

    initial commit

---
+++ sdi202-lab-soap-sw-2/src/main/java/com/uniovi/Sdi202LabSoapSw2Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi202LabSoapSw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi202LabSoapSw2Application.class, args);
+	}
+
+}
